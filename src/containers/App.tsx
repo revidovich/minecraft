1a19f4429f487287f2af7028a8f929a0147877f2
Anna <nutkatuz@yandex.ru>
2023-01-13 16:06:05 +0500

    styles

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,11 @@
 import { Provider } from 'react-redux';
 import { store } from '@/store';
+import '../main.css';
 
 export const App = () => {
     return (
         <Provider store={store}>
-            <>ghbdtn</>
+            <div id="map"></div>
         </Provider>
     );
 };
